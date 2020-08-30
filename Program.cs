44cc8d024faf58b63846afd1ea77f3c0201daf95
Matt Harrop <osaintspreserveus@live.com>
2020-08-30 07:58:10 -0700

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberRandomsToGenerate = 1;
+
+            if (args.Length > 0) {
+                try {
+                    int.TryParse(args[0], out numberRandomsToGenerate);
+                    if (numberRandomsToGenerate < 1) {
+                        Console.WriteLine("Please enter a valid number greater than 0 and rerun this application.");
+                        return;
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine("Please enter a valid number greater than 0 and rerun this application.");
+                }
+            }
+            for (int i = 0; i < numberRandomsToGenerate; i++) {
+                Random random = new Random();
+                int randomInt = random.Next();
+                Console.WriteLine(randomInt);
+            }
+        }
+    }
+}
